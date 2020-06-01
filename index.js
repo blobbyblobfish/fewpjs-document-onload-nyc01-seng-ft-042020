c7faa307135caeb09923e350d0b06a925c144175
blobbyblobfish <aywang6@gmail.com>
2020-06-01 17:20:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 // Your code goes here
 
-document.addEventListener( "DOMContentLoaded", function () {
-    updateDOM()
-  } );
+// document.addEventListener( "DOMContentLoaded", function () {
+//     updateDOM()
+//   } );
   
   
-  function updateDOM() {
-    document.getElementById( "text" )
-      .innerHTML = "This is really cool!";
-  }
+//   function updateDOM() {
+//     document.getElementById( "text" )
+//       .innerHTML = "This is really cool!";
+//   }
+
+ document.addEventListener("DOMContentLoaded", function (){
+   let p = document.querySelector("p#text")
+   p.textContent = "This is really cool!"
+ }) 
